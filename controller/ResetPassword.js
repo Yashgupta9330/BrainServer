4af81c1da27545389d56f1fb1a02344c8103e5b5
Yashgupta9330 <yaahg342@gmail.com>
2024-05-05 18:47:45 +0530

    added

--- controller/ResetPassword.js
+++ controller/ResetPassword.js
@@ -25,7 +25,7 @@ exports.resetPasswordToken = async (req, res) => {
 			{ new: true }
 		);
 		console.log("DETAILS", updatedDetails);
-		const url = `http://localhost:5173/update-password/${token}`;
+		const url = `https://brainopclient.vercel.app/update-password/${token}`;
 
 		await mailSender(
 			email,
